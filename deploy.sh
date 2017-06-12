08180fd52c0440b80a18fbe70e4ee606578b2924
zhengyuanjie <sj82516@gmail.com>
2017-06-12 21:07:03 +0800

    update CD

--- deploy.sh
+++ deploy.sh
@@ -3,6 +3,6 @@
 # Exit on any error
 set -e
 
-sudo /opt/google-cloud-sdk/bin/gcloud docker push gcr.io/koa-graphql-blog/mysite
+sudo /opt/google-cloud-sdk/bin/gcloud docker -- push gcr.io/koa-graphql-blog/mysite
 sudo chown -R ubuntu:ubuntu /home/ubuntu/.kube
 kubectl patch deployment mysite-dev -p '{"spec":{"template":{"spec":{"containers":[{"name":"mysite-dev","image":"gcr.io/koa-graphql-blog/mysite:'"$CIRCLE_SHA1"'"}]}}}}'
